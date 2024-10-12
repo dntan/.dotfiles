2eec12a0783c3de281c142e6901bff6e8956fdb8
danny tan <dn-tan@hotmail.com>
2024-10-12 13:12:56 +1100

    updated packages and included evil-collection for use with magit.

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -1,3 +1,21 @@
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
 ;;This is Danny's emacs config file as init.el
 
       (setq inhibit-startup-message t)
@@ -34,12 +52,19 @@
 (set-face-attribute 'default nil :font "DejaVu Sans" :height 130)
 
 ;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
+  (unless (package-installed-p 'evil)
+    (package-install 'evil))
 
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
+  (setq evil-want-keybinding nil)  
+  ;; Enable Evil
+  (require 'evil)
+  (evil-mode 1)
+
+(use-package evil-collection
+  :ensure t
+  :after evil
+  :init
+  (evil-collection-init))
 
 ;;some elisp code go here
 
@@ -58,23 +83,8 @@
 
 (setq org-format-latex-options (plist-put org-format-latex-options :scale 2.5))
 
-;; Initialize package sources
-(require 'package)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents
- (package-refresh-contents))
-
-;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-   (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
+(use-package magit
+:ensure t)
 
 (load-theme 'tango-dark)
 
